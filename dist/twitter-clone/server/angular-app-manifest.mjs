81a0a76a7ad865adf9cb55fc84872032a54de72d
urbanus-hub <urbankollyk@gmail.com>
2025-03-31 10:13:29 +0300

    dist added

--- dist/twitter-clone/server/angular-app-manifest.mjs
+++ dist/twitter-clone/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 23609, hash: '74e1304d50237f9610dfd0348986fa3cd2c637b3a5034d81681d1affe25adeee', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17140, hash: '691d76afd8b5935907d13ef1579d7bb98a7bae0502104bafa9e723992bc07909', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23609, hash: 'f437d63ba6361b9340258a3165b9add6c0728d847e2170e6a9f6688768e63864', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17140, hash: '178d056d65d8c68a4d9d9f85cafe2eb1099e0a47dea74019327d1afdeeeab997', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-MKKINXLT.css': {size: 7022, hash: '+aZsDhyJibo', text: () => import('./assets-chunks/styles-MKKINXLT_css.mjs').then(m => m.default)}
   },
 };
